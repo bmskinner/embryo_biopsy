49c7a0a1ae82804b34352d0f5fec8bdfd51b8abf
Ben Skinner <b.skinner@essex.ac.uk>
2021-06-05 20:44:58 +0100

    Add example plots for processing simulation data

--- examine.R
+++ examine.R
@@ -7,19 +7,28 @@ filt = data %>% dplyr::rowwise() %>%
   dplyr::mutate(n.euploid   = length(output[output=="0"])) %>%
   dplyr::mutate(n.aneuploid = length(output)-n.euploid,
                 PctEuploid  = n.euploid/length(output)*100
-                ) %>%
+  ) %>%
   dplyr::group_by(disps, aneuploids, cells, samples) %>%
-  dplyr::mutate(MeanEuploid = mean(PctEuploid),
-                SdEuploid   = sd(PctEuploid))
+  dplyr::summarise(MeanEuploid = mean(PctEuploid),
+                   SdEuploid   = sd(PctEuploid)) 
+
+# ggplot(filt, aes(x=aneuploids, y=MeanEuploid, fill=as.factor(disps), col=as.factor(disps)))+
+#   geom_ribbon(aes(ymin=0, ymax=MeanEuploid))+
+#   labs(x="Proportion of aneuploid cells", y="All euploid biopsies")+
+#   scale_color_viridis_d()+
+#   scale_fill_viridis_d()+
+#   theme_classic()+
+#   facet_grid(samples~cells)
 
 
-ggplot(filt, aes(x=samples, y=aneuploids, fill=MeanEuploid))+
+ggplot(filt %>% filter(aneuploids>0 & aneuploids<1), aes(x=cells, y=samples, fill=MeanEuploid>=50))+
   geom_tile()+
-  scale_fill_viridis_c()+
-  facet_grid(disps~cells)
-
-
-ggplot(filt, aes(x=aneuploids, y=MeanEuploid, col=samples, group=aneuploids))+
-  geom_point()+
-  geom_line()+
-  facet_grid(disps~cells)
+  scale_fill_viridis_d()+
+  labs(x="Cells in blastocyst", y="Cells biopsied", fill=">=50% chance of finding all euploid")+
+  facet_grid(disps~aneuploids)
+# 
+# 
+# ggplot(filt, aes(x=aneuploids, y=MeanEuploid, col=samples, group=aneuploids))+
+#   geom_point()+
+#   geom_line()+
+#   facet_grid(disps~cells)
