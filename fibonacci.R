6bf3d1b2fda5ad77f8c35f10dbf0427557101450
Ben Skinner <b.skinner@essex.ac.uk>
2021-07-01 08:45:06 +0100

    Add documentation

--- fibonacci.R
+++ fibonacci.R
@@ -4,19 +4,21 @@
 # The number of cells to be considered neighbours to a given cell
 N_NEIGHBOURS=6
 
-# Create a sphere of evenly spaced points 
+# Create a sphere of evenly spaced points
 # n.points - the number of points to create
 create.blank.sphere = function(n.points){
   # Make a sphere of evenly spaced points using the Fibonacci spiral
   indices = seq(0, n.points-1, 1)+0.5
+  # Map to a sphere via cylindrical equal area projection
   phi = acos(pmin(pmax( 1-2*indices/n.points,-1.0),1.0)) # constrain to avoid rounding errors
   theta = pi * (1 + sqrt(5)) * indices
-  
+
+  # Transform spherical coordinates to Cartesian
   x = cos(theta)*sin(phi)
   y = sin(theta)*sin(phi)
   z = cos(phi)
   d = as.data.frame(cbind(x, y, z))
-  
+
   # Create distance matrix for each point
   # Set the N_NEIGHBOURS closest points to be neighbours
   for(i in 1:nrow(d)){
@@ -26,7 +28,7 @@ create.blank.sphere = function(n.points){
     d[[paste0("isNeighbour", i)]] = dist > 0 & dist <= max(head(sort(dist), n=N_NEIGHBOURS+1))
   }
   d$isSeed = FALSE
-  
+
   # assertthat::assert_that(nrow(d)==n.points, msg=paste("Expected", n.points, "cells, found", nrow(d)))
   return(d)
 }
@@ -60,26 +62,26 @@ count.empty.blocks = function(d){
 create.blastocyst = function(n.cells, prop.aneuploid, dispersion){
 
   d = create.blank.sphere(n.cells)
-  
+
   # Shortcut the easy cases
   if(prop.aneuploid==0) return(d)
-  
+
   if(prop.aneuploid==1){
     d$isSeed=T
     return(d)
-  } 
-  
+  }
+
   # We must have an integer value of at least one aneuploid cell
   n.aneuploid = ceiling(max(1, n.cells * prop.aneuploid))
-  
-  # The approach for dispersal is to set seed cells which will 
+
+  # The approach for dispersal is to set seed cells which will
   # grow into separate aneuploid patches. The more dispersion, the more
   # initial seeds.
-  
+
   # Choose number of seeds for aneuploid regions
   n.seeds = ceiling(max(1, n.aneuploid * dispersion))
   n.to.make = n.seeds
-  
+
   # We can disperse up to a certain number of initial blocks with
   # no aneuploid neighbours. After this, every cell will have at least
   # one aneuploid neighbour. We stop a bit before this to make the maths simpler.
@@ -94,7 +96,7 @@ create.blastocyst = function(n.cells, prop.aneuploid, dispersion){
     n.to.make = n.to.make-1L
     initial.blocks = initial.blocks-1L
   }
-  
+
   # When all dispersed seeds have been added, add the remaining seeds randomly
   while(n.to.make>0){
     seed = sample.int(n.cells, 1)
@@ -102,9 +104,9 @@ create.blastocyst = function(n.cells, prop.aneuploid, dispersion){
     d$isSeed[seed] = T
     n.to.make = n.to.make-1L
   }
-  # assertthat::assert_that(sum(d$isSeed)==n.seeds, 
+  # assertthat::assert_that(sum(d$isSeed)==n.seeds,
                           # msg = paste("Expected", n.seeds, "seeds, found", sum(d$isSeed)))
-  
+
   # Grow the seeds into neighbouring cells for remaining aneuploid cells
   n.to.make = n.aneuploid - n.seeds
   while(n.to.make>0){
@@ -114,25 +116,25 @@ create.blastocyst = function(n.cells, prop.aneuploid, dispersion){
     d$isSeed[seed] = T
     n.to.make = n.to.make-1
   }
-  # assertthat::assert_that(sum(d$isSeed)==n.aneuploid, 
+  # assertthat::assert_that(sum(d$isSeed)==n.aneuploid,
   #                         msg = paste("Expected", n.aneuploid, "aneuploids, found", sum(d$isSeed)))
   return(d)
 }
 
-# Take a sample from a blastocyst. The cell at the given index is taken, 
+# Take a sample from a blastocyst. The cell at the given index is taken,
 # plus the closest n neighbouring cells where n = n.sampled.cells-1.
 # d - the blastocyst matrix
 # n.sampled.cells - the number of cells to biopsy
 # seed.sample - the index of the cell to begin biopsying
 sample.blastocyst = function(d, n.sampled.cells, seed.sample){
-  
+
   sample.list = d[[paste0("d", seed.sample)]]
-  
+
   d$isSampled = d[[paste0("d", seed.sample)]] <= max(head(sort(sample.list), n=n.sampled.cells))
   return(d)
 }
 
-# Find the number of aneuploid cells in all biopsies of 
+# Find the number of aneuploid cells in all biopsies of
 # a given size for the given blastocyst
 # d - the blastocyst matrix
 # n.cells.per.sample - the number of cells to take in each biopsy
