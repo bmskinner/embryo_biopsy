b375727f5ed1f780129a9bbc28df92de88875e4a
Ben Skinner <b.skinner@essex.ac.uk>
2023-05-12 15:14:47 +0100

    Add odds ratio calculations

--- processClinicalData.R
+++ processClinicalData.R
@@ -165,23 +165,7 @@ birth.combined.data <- data %>%
 
 ################################################################################
 
-# Check aneuploidy and segmntal type in glm
-imp.glm <- glm(implantation_sac ~ level_aneuploidy + seg_type, family = "binomial", data = data)
-
-imp.glm <- glm(implantation_sac ~ level_aneuploidy + seg_type, family = binomial(link = "logit"), data = data)
-summary(imp.glm)
-# Is the model useful? Strong lack of support for the null hypothesis
-pchisq(imp.glm$null.deviance - imp.glm$deviance, imp.glm$df.null - imp.glm$df.residual, lower.tail = F)
-
-# What does the model explain?
-deviance.diff <- imp.glm$null.deviance - imp.glm$deviance
-deviance.diff / imp.glm$null.deviance * 100
-# Very little of the variation (~1%) is explained by this model
-
-
-################################################################################
-
-# make p-vals for the glm to display on each chart
+# make p-vals for linear regressions to display on each chart
 imp.whole.glm <- glm(p_implanted_embryos ~ as.integer(bin), data = imp.data %>% dplyr::filter(seg_type == "Whole chromosome"))
 imp.whole.pval <- round(coef(summary(imp.whole.glm))[2, 4], digits = 4)
 
@@ -202,6 +186,8 @@ birth.all.pval <- round(coef(summary(birth.all.glm))[2, 4], digits = 4)
 
 
 ################################################################################
+
+# Create Viotti et al 2021 Fig2A row 1 equivalent
 
 YMAX.PCT <- 70
 
@@ -336,37 +322,6 @@ combined.plot <- ggplot(imp.combined.data, aes(x = as.integer(bin), y = p_implan
 
 ################################################################################
 
-
-# Make a logistic plot to show the full glm data
-
-
-# Predict values using the model
-new.data <- expand.grid(
-  level_aneuploidy = seq(20, 80, 5),
-  seg_type = c("Whole chromosome", "Segmental only")
-)
-
-preds <- predict(imp.glm, newdata = new.data, type = "response", se = T)
-new.data$pred.full <- preds$fit
-new.data$ymin <- new.data$pred.full - 2 * preds$se.fit
-new.data$ymax <- new.data$pred.full + 2 * preds$se.fit
-
-# Plot the real data and add the predicted values & error
-
-glm.fig <- ggplot() +
-  geom_jitter(data = data, aes(x = level_aneuploidy, y = as.integer(implantation_sac) - 1), height = 0.1) +
-  geom_ribbon(data = new.data, aes(x = level_aneuploidy, y = pred.full, ymin = ymin, ymax = ymax), alpha = 0.25) +
-  geom_line(data = new.data, aes(x = level_aneuploidy, y = pred.full), color = "black", linewidth = 2) +
-  scale_y_continuous(limits = c(-0.1, 1.1), breaks = seq(0, 1, 0.2)) +
-  facet_wrap(~seg_type) +
-  labs(x = "Mosaic level (%)", y = "Implantation success") +
-  theme_bw()
-
-save.double.width(glm.fig, filename = "figure/Figure_9_implantation_glm", height = 85)
-
-
-################################################################################
-
 # Recreate Viotti et al Fig 2A row 2: split into two groups, aneuploidy <50 or >=50%
 # Compare rates of implantation or OP/B in each group
 
@@ -421,14 +376,31 @@ combined.halves.data <- rbind(imp.halves.data, birth.halves.data) %>%
   )
 
 
+# combine the whole and segmental results together and bind
+all.halves.data <- combined.halves.data %>%
+  dplyr::ungroup() %>%
+  dplyr::group_by(group, Type) %>%
+  dplyr::summarise(
+    seg_type = "All",
+    total_embryos = sum(total_embryos),
+    n_embryos = sum(n_embryos),
+    p_outcome = n_embryos / total_embryos * 100,
+    x_embryo = total_embryos - n_embryos,
+    cgroup = paste0(Type, group)
+  ) %>%
+  dplyr::distinct() %>%
+  rbind(., combined.halves.data)
+
+
 # Test associations
 tests <- expand.grid(
-  seg_type = c("Whole chromosome", "Segmental only"),
+  seg_type = c("Whole chromosome", "Segmental only", "All"),
   Type = c("Implantation", "OP/B")
 )
 
 run.test <- function(s, t) {
-  filt <- combined.halves.data %>%
+  filt <- all.halves.data %>%
+    dplyr::ungroup() %>%
     dplyr::filter(seg_type == s & Type == t) %>%
     dplyr::select(n_embryos, x_embryo)
   ch <- chisq.test(filt)
@@ -464,11 +436,11 @@ whole.lines.data <- data.frame(
   ),
   "ystart" = c(
     44.25, 46.25, 46.25,
-    36, 40, 40
+    36, 38, 38
   ),
   "yend" = c(
     46.25, 46.25, 34,
-    40, 40, 22
+    38, 38, 22
   )
 )
 
@@ -522,11 +494,11 @@ seg.lines.data <- data.frame(
   ),
   "ystart" = c(
     54, 55, 55,
-    45, 55, 55
+    45, 47, 47
   ),
   "yend" = c(
     55, 55, 49,
-    55, 55, 39
+    47, 47, 39
   )
 )
 
@@ -597,7 +569,7 @@ both.halves.fig <- ggplot(both.halves.data, aes(x = cgroup, y = p_outcome)) +
   geom_text(aes(y = p_outcome - 5, label = round(p_outcome, digits = 2)), size = 2) +
   geom_text(aes(y = 5, label = paste0("n=", round(total_embryos, digits = 2))), size = 2) +
   geom_text(
-    data = tests, aes(y = 58, x = 1.5, label = paste0("p=", round(p.adj, digits = 4))),
+    data = tests[tests$seg_type == "All", ], aes(y = 58, x = x, label = paste0("p=", round(p.adj, digits = 4))),
     size = 2
   ) +
   labs(y = "Positive outcome (%)", title = "All mosaics") +
@@ -617,277 +589,105 @@ both.halves.fig <- ggplot(both.halves.data, aes(x = cgroup, y = p_outcome)) +
   )
 
 
-fig9.full <- (combined.plot + whole.plot + seg.plot) / (both.halves.fig + whole.halves.fig + seg.halves.fig) + plot_annotation(tag_levels = c("A"))
+fig9.full <- (combined.plot + whole.plot + seg.plot) / (both.halves.fig + whole.halves.fig + seg.halves.fig) +
+  plot_annotation(tag_levels = list(c("A", "", "", "B", "", "")))
 save.double.width(fig9.full, filename = "figure/Figure_9_full", height = 140)
 
 
 
 
-
-
 ################################################################################
 
-# Implantation by PGDIS classes
+# Create glms to test associations between aneuploidy, segmentation type and outcomes
 
-imp.pgdis.data <- data %>%
-  dplyr::group_by(pgdis_class) %>%
-  dplyr::mutate(total_embryos = n()) %>%
-  dplyr::group_by(pgdis_class, implantation_sac) %>%
+# Check aneuploidy and segmental type in glm
+imp.glm <- glm(implantation_sac ~ level_aneuploidy + seg_type, family = binomial(link = "logit"), data = data)
+imp.sum <- broom::tidy(imp.glm) %>%
   dplyr::mutate(
-    implanated_embryos = n(),
-    f_implanted_embryos = implanated_embryos / total_embryos
-  ) %>%
-  dplyr::select(pgdis_class, implanated_embryos, f_implanted_embryos) %>%
-  dplyr::distinct()
+    odds.ratio = exp(1)^estimate, # e^<U+03B2>
+    odds.ci.min = exp(1)^(estimate - 1.96 * std.error),
+    odds.ci.max = exp(1)^(estimate + 1.96 * std.error),
+    odds.dec = (odds.ratio - 1) * 100,
+    odds.ci.min.pct = (odds.ci.min - 1) * 100,
+    odds.ci.max.pct = (odds.ci.max - 1) * 100,
+  ) # e(<U+03B2> +/- 1.96*std error).
 
-imp.pgdis.plot <- ggplot(imp.pgdis.data, aes(x = pgdis_class, y = f_implanted_embryos)) +
-  geom_col() +
-  labs(x = "PGDIS class", y = "Fraction of embryos implanted") +
-  theme_classic() +
-  theme(legend.position = "none")
+# Is the model useful? Strong lack of support for the null hypothesis
+pchisq(imp.glm$null.deviance - imp.glm$deviance, imp.glm$df.null - imp.glm$df.residual, lower.tail = F)
 
-imp.pgdis.no.seg.data <- data %>%
-  dplyr::group_by(pgdis_class) %>%
-  dplyr::mutate(total_embryos = n()) %>%
-  dplyr::group_by(pgdis_class, implantation_sac) %>%
+# What does the model explain?
+imp.deviance.diff <- imp.glm$null.deviance - imp.glm$deviance
+imp.deviance.diff / imp.glm$null.deviance * 100
+# Very little of the variation (~1%) is explained by this model
+
+
+opb.glm <- glm(ongoing_birth ~ level_aneuploidy + seg_type, family = binomial(link = "logit"), data = data)
+summary(opb.glm)
+
+opb.sum <- broom::tidy(opb.glm) %>%
   dplyr::mutate(
-    implanated_embryos = n(),
-    f_implanted_embryos = implanated_embryos / total_embryos
-  ) %>%
-  dplyr::select(pgdis_class, implanated_embryos, f_implanted_embryos) %>%
-  dplyr::distinct() %>%
-  dplyr::filter(implantation_sac == 1)
+    odds.ratio = exp(1)^estimate, # e^<U+03B2>
+    odds.ci.min = exp(1)^(estimate - 1.96 * std.error),
+    odds.ci.max = exp(1)^(estimate + 1.96 * std.error),
+    odds.dec = (odds.ratio - 1) * 100,
+    odds.ci.min.pct = (odds.ci.min - 1) * 100,
+    odds.ci.max.pct = (odds.ci.max - 1) * 100,
+  ) # e(<U+03B2> +/- 1.96*std error).
 
-imp.pgdis.no.seg.plot <- ggplot(imp.pgdis.no.seg.data, aes(x = pgdis_class, y = f_implanted_embryos)) +
-  geom_col() +
-  labs(x = "PGDIS class", y = "Fraction of embryos implanted") +
-  theme_classic() +
-  theme(legend.position = "none")
+# Is the model useful? Strong lack of support for the null hypothesis
+pchisq(opb.glm$null.deviance - opb.glm$deviance, opb.glm$df.null - opb.glm$df.residual, lower.tail = F)
 
 
+# What does the model explain?
+opb.deviance.diff <- opb.glm$null.deviance - opb.glm$deviance
+opb.deviance.diff / opb.glm$null.deviance * 100
+# Very little of the variation (~1%) is explained by this model
 
-# Implantation by split classes
-imp.split.data <- data %>%
-  dplyr::group_by(split_class) %>%
-  dplyr::mutate(total_embryos = n()) %>%
-  dplyr::group_by(split_class, implantation_sac) %>%
-  dplyr::mutate(
-    implanated_embryos = n(),
-    f_implanted_embryos = implanated_embryos / total_embryos
-  ) %>%
-  dplyr::select(split_class, implanated_embryos, f_implanted_embryos) %>%
-  dplyr::distinct() %>%
-  dplyr::filter(implantation_sac == 1)
-
-imp.split.plot <- ggplot(imp.split.data, aes(x = split_class, y = f_implanted_embryos)) +
-  geom_col() +
-  labs(x = "50% split class", y = "Fraction of embryos implanted") +
-  theme_classic() +
-  theme(legend.position = "none")
-
-imp.split.no.seg.data <- data %>%
-  dplyr::group_by(split_class) %>%
-  dplyr::mutate(total_embryos = n()) %>%
-  dplyr::group_by(split_class, implantation_sac) %>%
-  dplyr::mutate(
-    implanated_embryos = n(),
-    f_implanted_embryos = implanated_embryos / total_embryos
-  ) %>%
-  dplyr::select(split_class, implanated_embryos, f_implanted_embryos) %>%
-  dplyr::distinct() %>%
-  dplyr::filter(implantation_sac == 1)
-
-imp.split.no.seg.plot <- ggplot(imp.split.no.seg.data, aes(x = split_class, y = f_implanted_embryos)) +
-  geom_col() +
-  labs(x = "50% split class", y = "Fraction of embryos implanted") +
-  theme_classic() +
-  theme(legend.position = "none")
-
-(imp.plot + imp.no.seg.plot) / (imp.pgdis.plot + imp.pgdis.no.seg.plot) / (imp.split.plot + imp.split.no.seg.plot) + plot_annotation(tag_levels = c("A"))
-
-
-imp.plot + imp.no.seg.plot
-
-# Split to all, whole chr and segmental mosaics
-
-################################################################################
-
-# Outcome results
-outcome.data <- data %>%
-  dplyr::group_by(bin) %>%
-  dplyr::mutate(total_embryos = n()) %>%
-  dplyr::group_by(bin, reported_outcome) %>%
-  dplyr::mutate(
-    f_outcome_embryos = n(),
-    f_outcome_embryos = f_outcome_embryos / total_embryos
-  ) %>%
-  dplyr::select(bin, reported_outcome, f_outcome_embryos) %>%
-  dplyr::distinct() %>%
-  dplyr::filter(reported_outcome != "n/a")
-
-ggplot(outcome.data, aes(x = bin, y = f_outcome_embryos)) +
-  geom_col() +
-  labs(x = "Aneuploidy (%)", y = "Fraction of embryos implanted") +
-  facet_wrap(~reported_outcome) +
-  theme_classic()
-
-# Outcome generic results
-outcome.data <- data %>%
-  dplyr::group_by(bin) %>%
-  dplyr::mutate(total_embryos = n()) %>%
-  dplyr::group_by(bin, generic_outcome) %>%
-  dplyr::mutate(
-    f_outcome_embryos = n(),
-    f_outcome_embryos = f_outcome_embryos / total_embryos
-  ) %>%
-  dplyr::select(bin, generic_outcome, f_outcome_embryos) %>%
-  dplyr::distinct() %>%
-  dplyr::filter(generic_outcome != "n/a")
-
-outcome.plot <- ggplot(outcome.data, aes(x = bin, y = f_outcome_embryos)) +
-  geom_col() +
-  labs(x = "Aneuploidy (%)", y = "Fraction of embryos with outcome") +
-  scale_y_continuous(limits = c(0, 1)) +
-  facet_wrap(~generic_outcome) +
-  theme_classic()
-
-# Outcome by PGDIS class
-# Outcome generic results
-pgdis.outcome.data <- data %>%
-  dplyr::group_by(pgdis_class) %>%
-  dplyr::mutate(total_embryos = n()) %>%
-  dplyr::group_by(pgdis_class, generic_outcome) %>%
-  dplyr::mutate(
-    f_outcome_embryos = n(),
-    f_outcome_embryos = f_outcome_embryos / total_embryos
-  ) %>%
-  dplyr::select(pgdis_class, generic_outcome, f_outcome_embryos) %>%
-  dplyr::distinct() %>%
-  dplyr::filter(generic_outcome != "n/a")
-
-
-pgdis.outcome.plot <- ggplot(pgdis.outcome.data, aes(x = pgdis_class, y = f_outcome_embryos)) +
-  geom_col() +
-  labs(x = "PGDIS class", y = "Fraction of embryos with outcome") +
-  scale_y_continuous(limits = c(0, 1)) +
-  facet_wrap(~generic_outcome) +
-  theme_classic()
-
-# Outcome by split class
-# Outcome generic results
-split.outcome.data <- data %>%
-  dplyr::group_by(split_class) %>%
-  dplyr::mutate(total_embryos = n()) %>%
-  dplyr::group_by(split_class, generic_outcome) %>%
-  dplyr::mutate(
-    f_outcome_embryos = n(),
-    f_outcome_embryos = f_outcome_embryos / total_embryos
-  ) %>%
-  dplyr::select(split_class, generic_outcome, f_outcome_embryos) %>%
-  dplyr::distinct() %>%
-  dplyr::filter(generic_outcome != "n/a")
-
-
-split.outcome.plot <- ggplot(split.outcome.data, aes(x = split_class, y = f_outcome_embryos)) +
-  geom_col() +
-  labs(x = "50% split class", y = "Fraction of embryos with outcome") +
-  scale_y_continuous(limits = c(0, 1)) +
-  facet_wrap(~generic_outcome) +
-  theme_classic()
-
-
-outcome.plot + pgdis.outcome.plot + split.outcome.plot + plot_annotation(tag_levels = c("A"))
 
 
 ################################################################################
 
 
-# Measure the contribution of class/bin to outcomes
+# Make logistic plots to show the glm data
 
-kruskal.test(generic_outcome ~ split_class, data = data)
-kruskal.test(reported_outcome ~ split_class, data = data)
-kruskal.test(generic_outcome ~ pgdis_class, data = data)
-kruskal.test(generic_outcome ~ bin, data = data)
+# Predict values using the model
+new.data <- expand.grid(
+  level_aneuploidy = seq(20, 80, 5),
+  seg_type = c("Whole chromosome", "Segmental only")
+)
 
-
-kruskal.test(implantation_sac ~ bin, data = data)
-kruskal.test(implantation_sac ~ pgdis_class, data = data)
-kruskal.test(implantation_sac ~ split_class, data = data)
-
-
-m <- glm(implantation_sac ~ level_aneuploidy, family = binomial(link = "logit"), data = data)
-summary(m)
-# Is the model useful to explain variation in measurements? Calc chi sq and convert to pvalue
-pchisq(m$null.deviance - m$deviance, 1, lower.tail = T)
-
-# Test using a multinomial model for reported outcome data
-glm.fit <- multinom(reported_outcome ~ level_aneuploidy, data = data)
-summary(glm.fit)
-
-# Separate test and training data
-alpha <- 0.7
-d <- sort(sample(nrow(data), nrow(data) * alpha))
-train <- data[d, ]
-test <- data[-d, ]
-glm.fit <- multinom(reported_outcome ~ level_aneuploidy, data = train)
-test$predict <- predict(glm.fit, test)
-
-test$correct <- test$predict == test$reported_outcome
-sum(test$correct) / length(test$correct)
+imp.preds <- predict(imp.glm, newdata = new.data, type = "response", se = T)
+new.data$imp.pred.full <- imp.preds$fit
+new.data$imp.ymin <- new.data$imp.pred.full - 2 * imp.preds$se.fit
+new.data$imp.ymax <- new.data$imp.pred.full + 2 * imp.preds$se.fit
 
 
-################################################################################
+opb.preds <- predict(opb.glm, newdata = new.data, type = "response", se = T)
+new.data$opb.pred.full <- opb.preds$fit
+new.data$opb.ymin <- new.data$opb.pred.full - 2 * opb.preds$se.fit
+new.data$opb.ymax <- new.data$opb.pred.full + 2 * opb.preds$se.fit
 
 
-# Look at fraction of euploid vs mosaic embryos vs outcomes
+# Plot the real data and add the predicted values & error
 
+glm.fig <- ggplot() +
+  geom_jitter(data = data, aes(x = level_aneuploidy, y = as.integer(implantation_sac) - 1), height = 0.1) +
+  geom_ribbon(data = new.data, aes(x = level_aneuploidy, y = imp.pred.full, ymin = imp.ymin, ymax = imp.ymax), alpha = 0.25) +
+  geom_line(data = new.data, aes(x = level_aneuploidy, y = imp.pred.full, colour = "Implantation"), linewidth = 2) +
+  geom_ribbon(data = new.data, aes(x = level_aneuploidy, y = opb.pred.full, ymin = opb.ymin, ymax = opb.ymax), fill = "red", alpha = 0.25) +
+  geom_line(data = new.data, aes(x = level_aneuploidy, y = opb.pred.full, colour = "Ongoing pregnancy / birth"), linewidth = 2) +
+  scale_y_continuous(limits = c(-0.1, 1.1), breaks = seq(0, 1, 0.2)) +
+  scale_color_manual(values = c("black", "red")) +
+  facet_wrap(~seg_type) +
+  labs(x = "Mosaic level (%)", y = "Probability of successful outcome") +
+  theme_bw() +
+  theme(
+    legend.title = element_blank(),
+    legend.position = c(0.15, 0.665),
+    legend.text = element_text(size = 8),
+    legend.background = element_blank(),
+    legend.key = element_blank(),
+    legend.key.size = unit(1.5, "mm")
+  )
 
-ggplot(data, aes(x = reported_outcome, y = f_mosaic_in_cycle)) +
-  geom_violin() +
-  geom_jitter() +
-  facet_wrap(~type)
-
-ggplot(data, aes(x = reported_outcome, y = f_euploid_in_cycle)) +
-  geom_violin() +
-  geom_jitter() +
-  facet_wrap(~type)
-ggplot(data, aes(x = reported_outcome, y = f_aneuploid_in_cycle)) +
-  geom_violin() +
-  geom_jitter() +
-  facet_wrap(~type)
-
-
-# Look at how total faction of mosaic embryos vary with aneuploidy levels
-
-# You don't have more mosaic embryos when aneuploidy is higher
-ggplot(data, aes(x = level_aneuploidy, y = f_mosaic_in_cycle)) +
-  geom_point() +
-  geom_smooth()
-
-
-ggplot(data, aes(x = level_aneuploidy, y = f_euploid_in_cycle)) +
-  geom_point() +
-  geom_smooth()
-ggplot(data, aes(x = level_aneuploidy, y = f_aneuploid_in_cycle)) +
-  geom_point() +
-  geom_smooth()
-
-################################################################################
-
-# How many double transfers?
-
-
-filt <- data %>%
-  filter(n_embryos_in_transfer > 1) %>%
-  dplyr::mutate(identity_deduced = str_detect(notes_on_transfer, "identity deduced")) %>%
-  filter(identity_deduced)
-
-ggplot(data, aes(x = reported_outcome, y = level_aneuploidy)) +
-  geom_violin() +
-  geom_jitter()
-
-ggplot(data, aes(x = type, y = level_aneuploidy)) +
-  geom_violin()
-
-ggplot(data, aes(x = type, y = level_aneuploidy)) +
-  geom_violin()
+save.double.width(glm.fig, filename = "figure/Figure_9_glm", height = 85)
